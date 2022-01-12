5a57355ca734e2afa0eba9bb3571b8866033fb65
Yglagioia <gonzalolagioia@gmail.com>
2022-01-12 20:14:49 -0300

    primer commit y configuracion de tailwinds

---
+++ instagram2/pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Instagram 2.0</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1 class="">Esto es INSTAGRAM 2.0</h1>
+      {/* {Header} */}
+
+      {/* {Feed} */}
+
+      {/* {Modal} */}
+
+    </div>
+  )
+}
