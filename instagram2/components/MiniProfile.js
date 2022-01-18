eb6d075b6486698dca63b0b97533916fc36b70fe
Yglagioia <gonzalolagioia@gmail.com>
2022-01-18 00:49:19 -0300

    mini profile changes

--- instagram2/components/MiniProfile.js
+++ instagram2/components/MiniProfile.js
@@ -1,14 +1,16 @@
 import { signOut, useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
 
 function MiniProfile() {
     const { data: session } = useSession()
+    const router = useRouter()
 
     return (
         <div className="flex items-center justify-between mt-14 ml-10">
             <img className="rounded-full border p-[2px] w-16 h-16" src={session?.user?.image} alt="user miniprofile img" />
 
             <div className="flex-1 mx-4">
-               <h2 className="font-medium">{session?.user?.username}</h2> 
+               <h2 onClick={() => router.push("/profile")} className="font-medium cursor-pointer">{session?.user?.username}</h2> 
                <h3 className="text-sm text-gray-400">{session?.user?.name}</h3>
             </div>
 
